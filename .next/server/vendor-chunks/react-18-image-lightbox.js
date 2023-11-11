3da541df53aa2145bed1a1400c27286baa8735bd
JOSE VARGAS <joseroberto-vargas@hotmail.com>
2023-11-11 13:38:28 -0500

    plantilla formateada

---
+++ .next/server/vendor-chunks/react-18-image-lightbox.js
@@ -0,0 +1,24 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/react-18-image-lightbox";
+exports.ids = ["vendor-chunks/react-18-image-lightbox"];
+exports.modules = {
+
+/***/ "./node_modules/react-18-image-lightbox/style.css":
+/*!********************************************************!*\
+  !*** ./node_modules/react-18-image-lightbox/style.css ***!
+  \********************************************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+};
+;
---
+++ next-i18next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    fallbackLng: 'en',
+  },
+  ssg: true // Set to true for Static Site Generation (SSG)
+};
